94f4a6d559451be3da559582d7f1743e81573af7
vincentbudianto <vincent.budianto@gmail.com>
2020-10-02 21:16:09 +0700

    + message bitplane

--- backend/imageLSB.py
+++ backend/imageLSB.py
@@ -87,9 +87,9 @@ class imageLSB():
         content = open(filename, "rb").read()
         data = len(content)
 
-        if (not self.randomized) and ((self.width * self.height * self.channels) < (filedata + data + 96)):
+        if ((not self.randomized) and ((self.width * self.height * self.channels) < (filedata + data + 96))):
             raise Exception('Image is smaller than payload')
-        elif (self.randomized) and ((self.width * self.height * self.channels) < (filedata + data + 128)):
+        elif ((self.randomized) and ((self.width * self.height * self.channels) < (filedata + data + 128))):
             raise Exception('Image is smaller than payload')
 
         if (self.encrypted):
@@ -168,3 +168,21 @@ class imageLSB():
         psnr = 20 * math.log10(max_pixel / math.sqrt(mse))
 
         return psnr
+
+if __name__ == '__main__':
+    image_encode = cv2.imread('image/input.png')
+    lsbe = imageLSB(image = image_encode, key = 'STEGANOGRAPHY', encrypted = False, randomized = True)
+
+    res_encode = lsbe.embed('image/secret.txt')
+    # res_encode = lsbe.embed('image/mask.png')
+    cv2.imwrite('image/res.png', res_encode)
+
+    image_decode = cv2.imread('image/res.png')
+    lsbd = imageLSB(image = image_decode, key = 'STEGANOGRAPHY')
+
+    res_decode = lsbd.extract()
+    # with open(res_decode[0], "wb") as f:
+    # 	f.write(res_decode[1])
+
+    # image_encode = cv2.imread('input.png')
+    # print('psnr :', lsbd.psnr(image_encode))
